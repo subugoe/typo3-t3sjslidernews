1cc10c73d6c77792000d8847b733ff1fc637f073
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-09-25 14:23:59 +0200

    Optimize for TYPO3 8

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,18 +4,19 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-function boot($key) {
+$boot = function ($key) {
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'pi1/class.tx_t3sjslidernews_pi1.php', '_pi1', 'list_type', 1);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($key, 'pi1/class.tx_t3sjslidernews_pi1.php', '_pi1',
+        'list_type', 1);
 
-$extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['t3s_jslidernews']);
+    $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['t3s_jslidernews']);
 
-if ($extConf['overlaySelected']) {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+    if ($extConf['overlaySelected']) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
       TCEFORM.tt_content.section_frame.addItems.84 = Content overlay (t3s_jslidernews)
   ');
-}
-}
+    }
+};
 
-boot('t3sjslidernews');
-
+$boot('t3sjslidernews');
+unset($boot);
--- ext_tables.php
+++ ext_tables.php
@@ -10,10 +10,10 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
     'LLL:EXT:t3s_jslidernews/locallang_db.xml:tt_content.list_type_pi1',
     $_EXTKEY.'_pi1',
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY).'ext_icon.gif',
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::siteRelPath($_EXTKEY).'ext_icon.gif',
 ], 'list_type');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/Resources/Private/FlexForms/flexform_ds.xml');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_content.pi_flexform.t3s_jslidernews_pi1.list', 'EXT:t3s_jslidernews/pi1/locallang_csh.xml');
 
